3a8674ccbc036e42149208a87eb6f4a90d0f21b7
Sanjiv <67266123+Sanjiv-Madhavan@users.noreply.github.com>
2024-04-30 15:06:25 +0530

    fixed deployment bugs and updated controller permissions

--- internal/controller/ddbctldpjob_controller.go
+++ internal/controller/ddbctldpjob_controller.go
@@ -18,6 +18,7 @@ package controller
 
 import (
 	"context"
+	"fmt"
 
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
@@ -41,6 +42,7 @@ type DdbctlDpJobReconciler struct {
 	Scheme *runtime.Scheme
 }
 
+//+kubebuilder:rbac:groups=batch,resources=jobs,verbs=create;get;list;watch;delete
 //+kubebuilder:rbac:groups=dynamoctl.dp.operators.sanjivmadhavan.io,resources=ddbctldpjobs,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=dynamoctl.dp.operators.sanjivmadhavan.io,resources=ddbctldpjobs/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=dynamoctl.dp.operators.sanjivmadhavan.io,resources=ddbctldpjobs/finalizers,verbs=update
@@ -70,7 +72,7 @@ func (r *DdbctlDpJobReconciler) Reconcile(ctx context.Context, req ctrl.Request)
 		Containers: []corev1.Container{
 			{
 				Name:    "ddbctl-delete-partition",
-				Image:   "sanjivmadhavan/go-dynamodb-partition-delete:latest",
+				Image:   "sanshunoisky/go-dynamoctl-deletepartition:0.0.1",
 				Command: []string{"/dynamoctl", "delete-partition"},
 				Args: []string{
 					"-t",
@@ -109,7 +111,9 @@ func (r *DdbctlDpJobReconciler) Reconcile(ctx context.Context, req ctrl.Request)
 		return ctrl.Result{}, err
 	}
 
-	if err := r.Create(ctx, &ddbctldpjob); err != nil {
+	fmt.Println(ddbctldpjob)
+
+	if err := r.Create(ctx, jobTemplate); err != nil {
 		log.Error(err, "unable to create Job for DeleteTablePartitionJob")
 		return ctrl.Result{}, err
 	}
